ff79701361d7b067c41976fb35c7a272da56c785
Leon <leon.heilmann@gmx.de>
2025-06-01 19:02:09 +0200

    feat: implement utils class (WIP)

---
+++ utils.lua
@@ -0,0 +1,169 @@
+local utils = {}
+
+utils.SCROLL_SPEED = 8
+utils.SYMBOL_SIZE = 30
+utils.REEL_WIDTH = 41
+utils.screen_offset_x = 28
+utils.screen_offset_y = 10
+utils.DISPLAY_TIME = 55
+
+utils.reels = {}
+utils.weighted_symbols = {}
+utils.symbol_names = { "A", "K", "Q", "J", "Ten", "Scarab", "Sungod", "Explorer", "Book" }
+utils.paylines = {
+    {2,2,2,2,2}, {1,1,1,1,1}, {3,3,3,3,3}, {1,2,3,2,1}, {3,2,1,2,3},
+    {2,3,3,3,2}, {2,1,1,1,2}, {3,3,2,1,1}, {1,1,2,3,3}, {3,2,2,2,1}
+}
+utils.symbol_values = {
+    Ten={0,5,25,100}, J={0,5,25,100}, Q={0,5,25,100}, K={0,5,40,150}, A={0,5,40,150},
+    Scarab={5,30,100,750}, Sungod={5,30,100,750}, Explorer={10,100,1000,5000}, Book={0,20,200,250}
+}
+utils.symbol_chances = { Ten=15, J=15, Q=15, K=12, A=12, Scarab=10, Sungod=8, Explorer=5, Book=3 }
+for s, w in pairs(utils.symbol_chances) do for _=1,w do table.insert(utils.weighted_symbols,s) end end
+
+utils.current_score = 0
+utils.total_score = 0
+utils.winning_lines = {}
+utils.auto_spin = false
+utils.quitting = false
+utils.spins_done = 0
+
+
+function utils.load_assets()
+    -- LOAD ASSETS
+    utils.background_img = Image.load("assets/sprites/Background.png", VRAM)
+    utils.background_img_lower = Image.load("assets/sprites/Background_lower.png", VRAM)
+
+    utils.hit_images = {}
+    utils.symbol_images = {}
+    for _, name in ipairs(utils.symbol_names) do
+        utils.symbol_images[name] = Image.load("assets/symbols/"..name..".png", VRAM)
+    end
+    for i=1,10 do
+        utils.hit_images[i] = Image.load("assets/symbols/crosses/"..i..".png", RAM)
+    end    
+end
+
+function utils.evaluate_bonus()
+    local books = 0
+    for i=1,5 do for j=1,3 do if utils.reels[i].symbols[j]=="Book" then books = books + 1 end end end
+    return books >= 3
+end
+
+function utils.evaluate_reels()
+    utils.current_score = 0
+    utils.winning_lines = {}
+
+    for idx, line in ipairs(utils.paylines) do
+        local first = utils.reels[1].symbols[line[1]]
+        local count, sym = 1, first
+
+        for i = 2, 5 do
+            local cur = utils.reels[i].symbols[line[i]]
+            if cur == sym or cur == "Book" or sym == "Book" then
+                count = count + 1
+                if cur ~= "Book" then sym = cur end
+            else
+                break
+            end
+        end
+
+        local is_high = sym == "Scarab" or sym == "Sungod" or sym == "Explorer"
+        if (count == 2 and is_high) or count >= 3 then
+            utils.current_score = utils.current_score + (utils.symbol_values[sym] and utils.symbol_values[sym][count - 1] or 0) * count
+            local positions = {}; for i=1, count do positions[i] = line[i] end
+            table.insert(utils.winning_lines, {line = idx, positions=positions})
+        end
+    end
+end
+
+function utils.handle_input()
+    Controls.read()
+    if Keys.newPress.Start then utils.quitting = true end
+    if Keys.newPress.B then utils.auto_spin = not utils.auto_spin end
+end
+
+function utils.draw_game_ui()
+    -- Upper screen
+    screen.blit(SCREEN_UP, 0, 0, utils.background_img)
+
+    -- Lower Background
+    screen.blit(SCREEN_DOWN, 0, 0, utils.background_img_lower)
+
+    -- === Game Stats ===
+    local text_color = Color.new256(0, 0, 0)
+    screen.print(SCREEN_DOWN, 170, 65, "" .. utils.current_score, text_color)
+    screen.print(SCREEN_DOWN, 170, 85, "" .. utils.total_score, text_color)
+    screen.print(SCREEN_DOWN, 170, 105, "" .. utils.spins_done, text_color)
+
+    local average_score = utils.spins_done > 0 and math.floor(utils.total_score / utils.spins_done) or 0
+    screen.print(SCREEN_DOWN, 170, 125, "" .. average_score, text_color)
+
+    -- === Bonus Symbol (in this case just an X) ===
+    screen.blit(SCREEN_DOWN, 170, 140, utils.hit_images[6])
+
+    -- === Controls ===
+    screen.drawFillRect(SCREEN_DOWN, 0, 180, 256, 192, Color.new256(0, 0, 0))
+    screen.print(SCREEN_DOWN, 40, 182, "A: Spin | B: Auto | START: Quit", Color.new256(200, 200, 200))
+
+    -- We also need to check user input here (yes, it's kinda ugly)
+    utils.handle_input()
+end
+
+function utils.display_reels()
+    utils.draw_game_ui()
+    for col = 1, 5 do
+        local x = utils.screen_offset_x + (col - 1) * utils.REEL_WIDTH + 2
+        for row = 0, 2 do
+            local sym = utils.reels[col].symbols[row + 1]
+            local y = utils.screen_offset_y + row * utils.SYMBOL_SIZE + 40 + (10 * row)
+            screen.blit(SCREEN_UP, x, y, utils.symbol_images[sym])
+        end
+    end
+    render()
+end
+
+function utils.render_winning_lines()
+
+    for index, line in ipairs(utils.winning_lines) do
+        local line_sound_played = false
+
+        for frame = 1, utils.DISPLAY_TIME do
+            utils.draw_game_ui()
+
+            -- Draw all symbols
+            for col = 1, 5 do
+                local reel = utils.reels[col]
+                local x = utils.screen_offset_x + (col - 1) * utils.REEL_WIDTH + 2
+
+                for row = 0, 2 do
+                    local sym = reel.symbols[row + 1]
+                    local y = utils.screen_offset_y + row * utils.SYMBOL_SIZE + 40 + 10 * row
+                    screen.blit(SCREEN_UP, x, y, utils.symbol_images[sym])
+                end
+            end
+
+            -- Highlight winning line
+            for col = 1, 5 do
+                local row = line.positions[col]
+                if row then
+                    local x = utils.screen_offset_x + (col - 1) * utils.REEL_WIDTH + 2
+                    local y = utils.screen_offset_y + (row - 1) * utils.SYMBOL_SIZE + 40 + 10 * (row - 1)
+
+                    if y >= 30 then
+                        if not line_sound_played then
+                            Sound.startSFX(math.random(0, 2))
+                            line_sound_played = true
+                        end
+                        screen.blit(SCREEN_UP, x, y, utils.hit_images[line.line])
+                    end
+                end
+            end
+
+            render()
+        end
+    end
+    utils.display_reels()
+end
+
+return utils
